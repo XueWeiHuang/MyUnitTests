2187800201402993db8621b509aba1d1b415efa0
Xue Wei Huang <chauvet1302@gmail.com>
2019-03-08 16:40:59 -0500

    Add project files.

---
+++ ActorControllerFunctionTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySQLWebserver.Controllers;
+using MySQLWebserver.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MyUnitTests
+{
+    public class ActorControllerFunctionTest
+    {
+        [Fact]
+        public void GetActorByIDSmokeTest()
+        {
+
+            var controller = new ActorController();
+            var result = controller.Get(101).Result as OkObjectResult;
+            var actor = result.Value as Actor;
+            Assert.Equal(actor.actor_id, 101);
+            Assert.Equal(actor.first_name, "SUSAN");
+            Assert.Equal(actor.last_name, "DAVIS");
+
+        }
+    }
+}
---
+++ ActorEndToEndTest.cs
@@ -0,0 +1,37 @@
+﻿using MySQLWebserver.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Xunit;
+
+namespace MyUnitTests
+{
+    public class ActorEndToEndTest
+    {
+        const string LOCALHOST = "http://localhost:5001/";
+        [Fact]
+        public async void GetActorByIdSmokeTest()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(LOCALHOST);
+                var acceptType = new MediaTypeWithQualityHeaderValue("application/json");
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(acceptType);
+                var response = await httpClient.GetAsync("api/actor/101");
+                string jsonString = null;
+                if (response.IsSuccessStatusCode)
+                {
+                    jsonString = await response.Content.ReadAsStringAsync();
+                    Assert.NotNull(jsonString);
+                   
+                }
+                Actor actor = JsonConvert.DeserializeObject<Actor>(jsonString);
+                Assert.NotNull
+            }
+        }
+    }
+}
